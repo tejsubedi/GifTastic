3fb67fbdc2698a45c4688e436ec53c3897c1d0bf
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-07-15 21:57:06 -0400

    Giphy api has been added

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+
+var animals = ["dog", "cat","rabit", "goldfish","bird","chicken"];
+
+function displayAnimalImage(){
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+  
+      });
+      //Creating the div for animals pic display
+      var animalDiv = $("<div class='movie'>");
+      //storing the rating of animal pic
+      var rating = response.data[0].rating;
+      var pRating = $("<p>").text(`Rating: ${rating}`);
+
+
+
+}
+
+
+
+    
